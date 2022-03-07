5fc0a2ac987c055a83a0f07dd77eda2574527f9b
faizulramir <faizul.ramir@gmail.com>
2022-03-07 13:01:48 +0800

    invoice submission to db

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -46,19 +46,25 @@ class InvoiceController extends Controller
     public function invoice_save(Request $request)
     {
         $json_data = json_decode(request()->post('jsonData'));
-        dd($json_data);
 
         if ($json_data) {
-            $inv_no = strtoupper($json_data->inv_no);
-            $inv_date = $json_data->inv_date;
-            $inv_company = strtoupper($json_data->inv_company);
-            $inv_remark = strtoupper($json_data->inv_remark);
-            $inv_status = strtoupper($json_data->inv_status);
-            $inv_total = $json_data->inv_total;
-            $inv_showtotal = $json_data->inv_showtotal;
-            $inv_entries = $json_data->entries;
-        }
-        else {
+            $invoice = new Invoice;
+            $invoice->inv_no = strtoupper($json_data->inv_no);
+            $invoice->inv_date = $json_data->inv_date;
+            $invoice->inv_company = strtoupper($json_data->inv_company);
+            $invoice->inv_remark = strtoupper($json_data->inv_remark);
+            $invoice->status = strtoupper($json_data->inv_status);
+            $invoice->inv_total = $json_data->inv_total;
+            $invoice->inv_showtotal = $json_data->inv_showtotal;
+            $invoice->json_inv = json_encode($json_data->entries, true);
+
+            $invoice->save();
+    
+            return response()->json([
+                'isSuccess' => true,
+                'Data' => 'Successfully Submitted!'
+            ], 200);
+        } else {
             return response()->json([
                 'isSuccess' => false,
                 'Data' => 'Data Incorrect. Failed to save Invoice!'
@@ -66,22 +72,6 @@ class InvoiceController extends Controller
         }
 
         //dd($inv_no, $inv_date, $inv_company, $inv_remark, $inv_total, $entries);
-
-        $invoice = new Invoice;
-        $invoice->inv_no = $inv_no;
-        $invoice->$inv_date = $inv_date;
-        $invoice->$inv_company = $inv_company;
-        $invoice->inv_remark = $inv_remark;
-        $invoice->inv_status = $inv_status;
-        $invoice->inv_showtotal = $inv_showtotal;
-        //$invoice->json_inv = $inv_entries;
-        
-        $invoice->save();
-
-        return response()->json([
-            'isSuccess' => true,
-            'Data' => 'Successfully Submitted!'
-        ], 200);
     }
 
 
